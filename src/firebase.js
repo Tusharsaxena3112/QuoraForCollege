75efc1b7db3ff18e2673b85457adc6d7edb8ddb7
Tusharsaxena3112 <saxenatushar2001@gmail.com>
2021-06-26 21:22:45 +0530

    changing database

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDM11HhaSZDM1Hk3gdetYgN0Jkzq5x_3q0",
-  authDomain: "quora-for-college-24095.firebaseapp.com",
-  projectId: "quora-for-college-24095",
-  storageBucket: "quora-for-college-24095.appspot.com",
-  messagingSenderId: "1094611885495",
-  appId: "1:1094611885495:web:02d7d19d8b36ea30421c32",
-  measurementId: "G-5NWJR7312T",
+  apiKey: "AIzaSyAp5Iz4KCWs7ENiQOME-WtdwoWSq1FGOPU",
+  authDomain: "quora-for-college-59061.firebaseapp.com",
+  projectId: "quora-for-college-59061",
+  storageBucket: "quora-for-college-59061.appspot.com",
+  messagingSenderId: "144842003513",
+  // appId: "1:1094611885495:web:02d7d19d8b36ea30421c32",
+  // measurementId: "G-5NWJR7312T",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
